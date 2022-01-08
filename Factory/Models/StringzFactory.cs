d27c0aa6baf06d2397ca9ca7e10745563452611f
Kim Brannian <brannian@gmail.com>
2022-01-08 10:23:25 -0800

    edit models to add stringzfactory

--- Factory/Models/StringzFactory.cs
+++ Factory/Models/StringzFactory.cs
@@ -2,16 +2,16 @@ using System.Collections.Generic;
 
 namespace Factory.Models
 {
-  public class Factory
+  public class StringzFactory
   {
-    public int FactoryId { get; set; }
+    public int StringzFactoryId { get; set; }
     public string Name { get; set; }
 
-    public string Address
+    public string Address { get; set; }
     public virtual ICollection<Engineer> Engineers { get; set; }
     public virtual ICollection<Machine> Machines { get; set; }
 
-    public Factory()
+    public StringzFactory()
     {
       this.Engineers = new HashSet<Engineer>();
       this.Machines = new HashSet<Machine>();
