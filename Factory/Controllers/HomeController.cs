fe9748c19f69d194117644ae88aaab6631e007aa
Kim Brannian <brannian@gmail.com>
2022-01-11 10:26:33 -0800

    add views to homecontroller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Dynamic;
 
 namespace Factory.Controllers
 {
+
+    
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
 
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+      
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewData["engineers"]= _db.Engineers.ToList();
+        ViewData["machines"] = _db.Machines.ToList();
         return View();
       }
 
+      }
+
     }
-}
